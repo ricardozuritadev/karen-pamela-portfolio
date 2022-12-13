53d9b1e6ffaa2dca2c53034081adb324f4c8d16c
rickyzu <ricardo.zurita.dev@gmail.com>
2022-12-14 00:02:29 +0100

    Modify ProjectCard components

--- src/components/ProjectsContainer/index.tsx
+++ src/components/ProjectsContainer/index.tsx
@@ -14,23 +14,43 @@ const ProjectsContainer = () => {
   const { t } = useTranslation();
   const { scrollYProgress } = useScroll();
 
-  const transformOnScroll: MotionValue = useTransform(
-    scrollYProgress,
-    [0, 1],
-    [transform, '#EDEDED', '#303030', '#161616']
-  );
+  // const transformOnScroll: MotionValue = useTransform(
+  //   scrollYProgress,
+  //   [0, 1],
+  //   [transform, '#EDEDED', '#303030', '#161616']
+  // );
 
   return (
     <div className="projects">
       <h2 className="projects__title">{t('APP.HOME.PROJECTS')}</h2>
       <div className="projects__container">
         <div>
-          <ProjectCard backgroundImg="zensave" />
-          <ProjectCard backgroundImg="kukara" />
+          <ProjectCard
+            backgroundImg="zensave"
+            title="Zensave"
+            description="App Design | Savings though payments & Retirement"
+            tags={['ux/ui designer', 'individual', '14 días']}
+          />
+          <ProjectCard
+            backgroundImg="kukara"
+            title="Kukaramakara"
+            description="App Design | Savings though payments & Retirement"
+            tags={['ux/ui designer', 'individual', '14 días']}
+          />
         </div>
         <div>
-          <ProjectCard backgroundImg="goodtrade" />
-          <ProjectCard backgroundImg="storytell" />
+          <ProjectCard
+            backgroundImg="goodtrade"
+            title="Good Trade"
+            description="App Design | Savings though payments & Retirement"
+            tags={['ux/ui designer', 'individual', '14 días']}
+          />
+          <ProjectCard
+            backgroundImg="storytell"
+            title="Storytell"
+            description="App Design | Savings though payments & Retirement"
+            tags={['ux/ui designer', 'individual', '14 días']}
+          />
         </div>
       </div>
     </div>
