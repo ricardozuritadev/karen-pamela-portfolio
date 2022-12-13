25ebd5e712df04a16306e59444ff793c99376eaf
rickyzu <ricardo.zurita.dev@gmail.com>
2022-12-13 23:14:05 +0100

    Import ProjectCard container

--- src/components/ProjectsContainer/index.tsx
+++ src/components/ProjectsContainer/index.tsx
@@ -1,9 +1,38 @@
+import {
+  motion,
+  useScroll,
+  useTransform,
+  MotionValue,
+  transform,
+} from 'framer-motion';
+
+import { useTranslation } from 'react-i18next';
+
 import ProjectCard from '../ProjectCard';
 
 const ProjectsContainer = () => {
+  const { t } = useTranslation();
+  const { scrollYProgress } = useScroll();
+
+  const transformOnScroll: MotionValue = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [transform, '#EDEDED', '#303030', '#161616']
+  );
+
   return (
-    <div className="projects-container">
-      <ProjectCard />
+    <div className="projects">
+      <h2 className="projects__title">{t('APP.HOME.PROJECTS')}</h2>
+      <div className="projects__container">
+        <div>
+          <ProjectCard backgroundImg="zensave" />
+          <ProjectCard backgroundImg="kukara" />
+        </div>
+        <div>
+          <ProjectCard backgroundImg="goodtrade" />
+          <ProjectCard backgroundImg="storytell" />
+        </div>
+      </div>
     </div>
   );
 };
