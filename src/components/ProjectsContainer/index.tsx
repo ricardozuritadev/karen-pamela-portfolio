75d3dd73f008b1ea1b92d99d01fa12d643448053
rickyzu <ricardo.zurita.dev@gmail.com>
2022-12-13 16:39:46 +0100

    Create ProjectsContainer component

---
+++ src/components/ProjectsContainer/index.tsx
@@ -0,0 +1,11 @@
+import ProjectCard from '../ProjectCard';
+
+const ProjectsContainer = () => {
+  return (
+    <div className="projects-container">
+      <ProjectCard />
+    </div>
+  );
+};
+
+export default ProjectsContainer;
