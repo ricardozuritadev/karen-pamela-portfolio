e21fed28111a2accd4c883070b5ee1782058b8f6
rickyzu <ricardo.zurita.dev@gmail.com>
2022-12-14 00:01:53 +0100

    Add project card descriptions

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,9 +1,31 @@
 type ProjectCardProps = {
   backgroundImg: string;
+  title: string;
+  description: string;
+  tags: string[];
 };
 
-const ProjectCard = ({ backgroundImg }: ProjectCardProps) => {
-  return <div className={`project-card project-card--${backgroundImg}`}></div>;
+const ProjectCard = ({
+  backgroundImg,
+  title,
+  description,
+  tags,
+}: ProjectCardProps) => {
+  return (
+    <div className={`project-card project-card--${backgroundImg}`}>
+      <div className={`project-card__details`}>
+        <h2 className="project-card__title">{title}</h2>
+        <p className="project-card__description">{description}</p>
+        <ul className="project-card__tags">
+          {tags.map((tag, i) => (
+            <li key={i} className="project-card__tag">
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
