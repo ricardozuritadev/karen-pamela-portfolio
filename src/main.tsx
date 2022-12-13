97fed61090764cee6bebae992985c5e85ef57642
rickyzu <ricardo.zurita.dev@gmail.com>
2022-12-13 16:39:09 +0100

    Import translations config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import './styles/styles.scss';
+import './configs/translations-config';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
