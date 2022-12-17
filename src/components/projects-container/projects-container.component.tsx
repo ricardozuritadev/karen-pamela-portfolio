8235f3969a4778e4873ea7ccbc2524cb4824b57c
rickyzu <ricardo.zurita.dev@gmail.com>
2022-12-17 19:27:37 +0100

    Modify projects container component

--- src/components/projects-container/projects-container.component.tsx
+++ src/components/projects-container/projects-container.component.tsx
@@ -1,10 +1,4 @@
-import {
-  motion,
-  useScroll,
-  useTransform,
-  MotionValue,
-  transform,
-} from 'framer-motion';
+import { motion, useScroll, useTransform, MotionValue } from 'framer-motion';
 
 import { useTranslation } from 'react-i18next';
 
@@ -29,44 +23,63 @@ const ProjectsContainer = () => {
       <h2 className="projects__title">{t('APP.HOME.PROJECTS')}</h2>
       <div className="projects__container">
         <div>
-          <ProjectCard
-            backgroundImg="zensave"
-            title="zensave-title"
-            description={t('APP.PROJECTS.ZENSAVE.DESCRIPTION')}
-            role={t('APP.TAGS.ROLE.DESIGNER')}
-            projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
-            duration={14}
-            tagsColor="yellow"
-          />
-          <ProjectCard
-            backgroundImg="kukara"
-            title="kukara-title"
-            description={t('APP.PROJECTS.KUKARA.DESCRIPTION')}
-            role={t('APP.TAGS.ROLE.DESIGNER')}
-            projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
-            duration={14}
-            tagsColor="cyan"
-          />
+          <a
+            href="https://karen-pamela.vercel.app/pages/zensave-case-study.html"
+            target="_blank"
+          >
+            <ProjectCard
+              backgroundImg="zensave"
+              title="zensave-title"
+              description={t('APP.PROJECTS.ZENSAVE.DESCRIPTION')}
+              role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
+              projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
+              duration={14}
+              tagsColor="cyan"
+            />
+          </a>
+
+          <a
+            href="https://karen-pamela.vercel.app/pages/kukara-case-study.html"
+            target="_blank"
+          >
+            <ProjectCard
+              backgroundImg="kukara"
+              title="kukara-title"
+              description={t('APP.PROJECTS.KUKARA.DESCRIPTION')}
+              role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
+              projectType={t('APP.TAGS.TYPE.TEAM')}
+              duration={8}
+              tagsColor="yellow"
+            />
+          </a>
         </div>
         <div>
-          <ProjectCard
-            backgroundImg="goodtrade"
-            title="goodtrade-title"
-            description={t('APP.PROJECTS.GOODTRADE.DESCRIPTION')}
-            role={t('APP.TAGS.ROLE.DESIGNER')}
-            projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
-            duration={14}
-            tagsColor="green"
-          />
-          <ProjectCard
-            backgroundImg="storytell"
-            title="storytell-title"
-            description={t('APP.PROJECTS.STORYTELL.DESCRIPTION')}
-            role={t('APP.TAGS.ROLE.DESIGNER')}
-            projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
-            duration={14}
-            tagsColor="turquoise"
-          />
+          <a
+            href="https://karen-pamela.vercel.app/pages/good-trade-case-study.html"
+            target="_blank"
+          >
+            <ProjectCard
+              backgroundImg="goodtrade"
+              title="goodtrade-title"
+              description={t('APP.PROJECTS.GOODTRADE.DESCRIPTION')}
+              role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
+              projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
+              duration={14}
+              tagsColor="purple"
+            />
+          </a>
+
+          <a href="#">
+            <ProjectCard
+              backgroundImg="storytell"
+              title="storytell-title"
+              description={t('APP.PROJECTS.STORYTELL.DESCRIPTION')}
+              role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
+              projectType={t('APP.TAGS.TYPE.TEAM')}
+              duration={5}
+              tagsColor="orange"
+            />
+          </a>
         </div>
       </div>
     </motion.div>
