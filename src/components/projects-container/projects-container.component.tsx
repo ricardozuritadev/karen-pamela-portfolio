2412c07a47996b86b990fb69364a0b0e1cbcefe3
rickyzu <ricardo.zurita.dev@gmail.com>
2022-12-19 08:17:43 +0100

    Implement parallax component

--- src/components/projects-container/projects-container.component.tsx
+++ src/components/projects-container/projects-container.component.tsx
@@ -1,88 +1,100 @@
-import { motion, useScroll, useTransform, MotionValue } from 'framer-motion';
-
+import { useInView } from 'react-intersection-observer';
 import { useTranslation } from 'react-i18next';
 
+import { motion } from 'framer-motion';
+
+import Parallax from '../parallax/parallax.component';
 import ProjectCard from 'components/project-card/project-card.component';
 
 const ProjectsContainer = () => {
   const { t } = useTranslation();
-  const { scrollYProgress } = useScroll();
 
-  const opacityOnScroll: MotionValue = useTransform(
-    scrollYProgress,
-    [0, 0.15],
-    [0, 1]
-  );
+  const [ref, inView, entry] = useInView({
+    threshold: 0.5,
+    triggerOnce: false,
+  });
 
-  const opacityMotionStyle = {
-    opacity: opacityOnScroll,
+  const variants = {
+    visible: { opacity: 1, scale: 1 },
+    hidden: {
+      opacity: 0,
+      scale: 1,
+    },
   };
 
   return (
-    <motion.div className="projects" style={opacityMotionStyle}>
-      <h2 className="projects__title">{t('APP.HOME.PROJECTS')}</h2>
-      <div className="projects__container">
-        <div>
-          <a
-            href="https://karen-pamela.vercel.app/pages/zensave-case-study.html"
-            target="_blank"
-          >
-            <ProjectCard
-              backgroundImg="zensave"
-              title="zensave-title"
-              description={t('APP.PROJECTS.ZENSAVE.DESCRIPTION')}
-              role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
-              projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
-              duration={14}
-              tagsColor="cyan"
-            />
-          </a>
+    <Parallax>
+      <motion.div
+        className="projects"
+        animate={inView ? 'visible' : 'hidden'}
+        variants={variants}
+        transition={{ duration: 1.5, ease: 'easeOut' }}
+        ref={ref}
+      >
+        <h2 className="projects__title">{t('APP.HOME.PROJECTS')}</h2>
+        <div className="projects__container">
+          <div>
+            <a
+              href="https://karen-pamela.vercel.app/pages/zensave-case-study.html"
+              target="_blank"
+            >
+              <ProjectCard
+                backgroundImg="zensave"
+                title="zensave-title"
+                description={t('APP.PROJECTS.ZENSAVE.DESCRIPTION')}
+                role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
+                projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
+                duration={14}
+                tagsColor="cyan"
+              />
+            </a>
 
-          <a
-            href="https://karen-pamela.vercel.app/pages/kukara-case-study.html"
-            target="_blank"
-          >
-            <ProjectCard
-              backgroundImg="kukara"
-              title="kukara-title"
-              description={t('APP.PROJECTS.KUKARA.DESCRIPTION')}
-              role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
-              projectType={t('APP.TAGS.TYPE.TEAM')}
-              duration={8}
-              tagsColor="yellow"
-            />
-          </a>
-        </div>
-        <div>
-          <a
-            href="https://karen-pamela.vercel.app/pages/good-trade-case-study.html"
-            target="_blank"
-          >
-            <ProjectCard
-              backgroundImg="goodtrade"
-              title="goodtrade-title"
-              description={t('APP.PROJECTS.GOODTRADE.DESCRIPTION')}
-              role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
-              projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
-              duration={14}
-              tagsColor="purple"
-            />
-          </a>
+            <a
+              href="https://karen-pamela.vercel.app/pages/kukara-case-study.html"
+              target="_blank"
+            >
+              <ProjectCard
+                backgroundImg="kukara"
+                title="kukara-title"
+                description={t('APP.PROJECTS.KUKARA.DESCRIPTION')}
+                role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
+                projectType={t('APP.TAGS.TYPE.TEAM')}
+                duration={8}
+                tagsColor="yellow"
+              />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://karen-pamela.vercel.app/pages/good-trade-case-study.html"
+              target="_blank"
+            >
+              <ProjectCard
+                backgroundImg="goodtrade"
+                title="goodtrade-title"
+                description={t('APP.PROJECTS.GOODTRADE.DESCRIPTION')}
+                role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
+                projectType={t('APP.TAGS.TYPE.INDIVIDUAL')}
+                duration={14}
+                tagsColor="purple"
+              />
+            </a>
 
-          <a href="#">
-            <ProjectCard
-              backgroundImg="storytell"
-              title="storytell-title"
-              description={t('APP.PROJECTS.STORYTELL.DESCRIPTION')}
-              role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
-              projectType={t('APP.TAGS.TYPE.TEAM')}
-              duration={5}
-              tagsColor="orange"
-            />
-          </a>
+            <a href="#">
+              <ProjectCard
+                backgroundImg="storytell"
+                title="storytell-title"
+                description={t('APP.PROJECTS.STORYTELL.DESCRIPTION')}
+                role={t('APP.TAGS.ROLE.UXUIDESIGNER')}
+                projectType={t('APP.TAGS.TYPE.TEAM')}
+                duration={5}
+                tagsColor="orange"
+              />
+            </a>
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </Parallax>
   );
 };
 
